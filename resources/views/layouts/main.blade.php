1045a9844b49b9713b2b100d0674469fd909afff
Jc-atktechnologies <jeremy.campbell@atktechnologies.ca>
2022-06-20 09:08:17 -0400

    Few css changes and sidebar as well

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,24 +4,20 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Klaims Manager</title>
-        <link rel="stylesheet" href="../resources/css/bootstrap.css" />
+        <link rel="stylesheet" href="../ressources/css/app.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>      
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     </head>
 
     <body>
-        <div id="container-fluid">
-            <div class="container">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <div class="row">
-                        <div class="col-md-3 col-sm-3 col-xs-3">
-                            @extends('layouts.sidebar')
-                        </div>
-                        <div class="col-md-9 col-sm-9 col-xs-9">
-                            @yield('content')
-                        </div>
-                    </div>
-                </div>
+        <div id="container">
+            <div id="nav-sidebar">
+                @extends('layouts.sidebar')
+            </div>
+            <div id="content">
+                @yield('content')
             </div>
         </div>
     </body>
