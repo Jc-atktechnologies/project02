75f7883a673b4674f1f794d438f10e4833599913
Jc-atktechnologies <jeremy.campbell@atktechnologies.ca>
2022-06-24 13:37:17 -0400

    color change

--- resources/views/main/claims.blade.php
+++ resources/views/main/claims.blade.php
@@ -1,41 +1,66 @@
 @extends ('layouts.app')
 
 @section('content')
-    <div id="container">
-        <div id="content">
-            <table class="table table-hover">
-                <thead class="thead-dark">
-                <tr>
-                    <th scope="col">ID</th>
-                    <th href="dossierinfo" scope="col"># Dossier</th>
-                    <th scope="col"># Police</th>
-                    <th scope="col"># Rec.</th>
-                    <th scope="col">Client</th>
-                    <th scope="col">Téléphone</th>
-                    <th scope="col">Adresse</th>
-                    <th scope="col">Adresse sinistre</th>
-                    <th scope="col">Assureur</th>
-                    <th scope="col">Réviseur</th>
-                </tr>
-                </thead>
 
-                <tbody>
-                <tr>
-                    <th scope="row">23561</th>
-                    <td >Example</td>
-                    <td>Data</td>
-                    <td>12345</td>
-                </tr>
 
-                <tr>
-                    <th scope="row">23362</th>
-                    <td >Example</td>
-                    <td>Data</td>
-                    <td>76592</td>
-                </tr>
+<div class="container-fluid" >
+  <div class="table-responsive">
+  <table class="table table-bordered table-striped table-hover">
+        <thead class="bg-secondary text-white">
+        <tr>
+            <th rowspan="2">Insurer</th>
+            <th rowspan="2">Insured</th>
+            <th rowspan="2">Claim #</th>
+            <th rowspan="2">Date Received</th>
+            <th rowspan="2">Loss date</th>
+            <th rowspan="2">Loss type</th>
+            <th rowspan="2">Status</th>
+            <th rowspan="2">Last activity</th>
+            <th rowspan="2">File age</th>
+            <th rowspan="2">Assigned</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+        </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+  </tbody>
+</table>
 
-                </tbody>
-            </table>
-        </div>
-    </div>
+</div>
+
+</div>
+
 @endsection
