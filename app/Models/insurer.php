7efacde46e58fd4536a9f653ab0957325e5bd9e6
Jc-atktechnologies <jeremy.campbell@atktechnologies.ca>
2022-07-02 18:38:21 -0400

    Update insurer.php

--- app/Models/insurer.php
+++ app/Models/insurer.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Insurer extends Model
 {
     use HasFactory;
