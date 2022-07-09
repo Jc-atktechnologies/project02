7620bf48fa88544825063da253c919469eda7175
saeed-awan303 <49744862+saeed-awan303@users.noreply.github.com>
2022-07-09 22:35:41 +0500

    added fillable in insurer model

--- app/Models/insurer.php
+++ app/Models/insurer.php
@@ -6,5 +6,20 @@ class Insurer extends Model
 {
     use HasFactory;
     
-    protected $guarded = [];
+    //protected $guarded = [];
+    protected $fillable =[
+                            'company_name',
+                            'branch_id',
+                            'insurer_address',
+                            'insurer_city',
+                            'insurer_province',
+                            'insurer_postal',
+                            'insurer_country',
+                            'insurer_phone',
+                            'insurer_fax',
+                            'insurer_email',
+                            'insurer_altphone',
+                            'insurer_altemail',
+                            'insurer_notes'
+                            ];
 }
